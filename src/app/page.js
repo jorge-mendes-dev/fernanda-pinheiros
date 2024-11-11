b4a6e1aed2be866f26cf97bfd7b5755d09c93aa0
Jorge Mendes <jorge.mendes@escala-app.com>
2024-11-11 10:59:50 -0300

    first version website - components import

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import { Suspense } from 'react'
-import Navigation from '@/components/Navigation'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-import About from '@/components/About'
-import Products from '@/components/Products'
-import FadeIn from '@/components/FadeIn'
-import Loader from '@/components/Loader'
+import Navigation from './components/Navigation'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import About from './components/About'
+import Products from './components/Products'
+import FadeIn from './components/FadeIn'
+import Loader from './components/Loader'
 
 export default function Home() {
   return (
