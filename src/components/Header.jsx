c7a052dc2feab5a474599f2757a24d782fe34b3e
Jorge Mendes <br0jmns@festo.net>
2025-09-10 17:55:43 -0300

    updates finish mvp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Image from 'next/image';
 import { AppConfig } from 'config/AppConfig';
 import { FaWhatsapp } from "react-icons/fa";
--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -1,16 +1,35 @@
 'use client';
 
+import { Suspense } from "react";
 import { AppConfig } from "config/AppConfig";
+import Loader from "./Loader";
+import { useEffect, useRef } from "react";
 
 export default function Instagram() {
+    const instagramRef = useRef(null);
+
+  useEffect(() => {
+    if (!document.getElementById("instagram-embed-script")) {
+      const script = document.createElement("script");
+      script.id = "instagram-embed-script";
+      script.src = "//www.instagram.com/embed.js";
+      script.async = true;
+      document.body.appendChild(script);
+    } else if (window.instgrm) {
+      window.instgrm.Embeds.process();
+    }
+  }, []);
+
   return (
-    <div className="sm:flex transition-shadow duration-300 mt-8 inline-block">
-      <blockquote
-        className="instagram-media"
-        data-instgrm-permalink={AppConfig.social.instagram}
-        data-instgrm-version="14"
-      ></blockquote>
-      <script async src="//www.instagram.com/embed.js"></script>
-    </div>
+    <Suspense fallback={<Loader />}>
+      <div className="sm:flex transition-shadow duration-300 mt-8 inline-block">
+        <blockquote
+          ref={instagramRef}
+          className="instagram-media"
+          data-instgrm-permalink={AppConfig.social.instagram}
+          data-instgrm-version="14"
+        ></blockquote>
+      </div>
+    </Suspense>
   );
 }
